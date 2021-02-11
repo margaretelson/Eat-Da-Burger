9383859726d26ddb058f6ab2a51d8b38192e5c67
Margaret Elson <margaret.a.elson@gmail.com>
2021-02-11 17:58:36 -0500

    connection.js created

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  // NOTE: Be sure to add your MySQL password here!
+  password: 'Dre525252',
+  database: 'burger_db',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+module.exports = connection;

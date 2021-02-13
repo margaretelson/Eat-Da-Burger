bba616d8b89a98a8055a92338e516d096753fe18
Margaret Elson <margaret.a.elson@gmail.com>
2021-02-13 07:43:48 -0500

    orm.js is for the most part finished. Still need to test to ensure it is working

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,6 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
   password: 'Dre525252',
   database: 'burgers_db',
 });
